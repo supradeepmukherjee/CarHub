8ddc577b7ee6a4ef876da9b856ce8859c27a6dc1
Supradeep Mukherjee <supradeep2004@gmail.com>
2025-05-09 19:43:55 +0530

    Project Setup

---
+++ src/auth.ts
@@ -0,0 +1,10 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { prisma } from "./lib/prisma";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+    providers: [GitHub],
+    adapter: PrismaAdapter(prisma),
+    session: { strategy: 'jwt' }
+})
